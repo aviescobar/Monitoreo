8cb2b31012d04e34995e5c39160c95b741dd028b
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-18 21:05:03 -0600

    Update apagar.py

--- Apagar/apagar.py
+++ Apagar/apagar.py
@@ -6,3 +6,5 @@ HOST = '0.0.0.0'  # Dirección IP del servidor
 PORT = 8080         # Puerto en el que el servidor escuchará
 
 # Lista de páginas prohibidas
+PAGINAS_PROHIBIDAS = ["/prohibido.html", "/privado.html"]
+

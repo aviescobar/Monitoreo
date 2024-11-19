7e69d354db979bf942d28b709c0b327ac752324f
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-18 21:04:45 -0600

    Update apagar.py

--- Apagar/apagar.py
+++ Apagar/apagar.py
@@ -4,3 +4,5 @@ import socket
 # Configuración del servidor
 HOST = '0.0.0.0'  # Dirección IP del servidor
 PORT = 8080         # Puerto en el que el servidor escuchará
+
+# Lista de páginas prohibidas

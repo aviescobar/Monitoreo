8889bd8b96f019c3a39d108c142230dcbf080789
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-18 21:14:56 -0600

    Update apagar.py

--- Apagar/apagar.py
+++ Apagar/apagar.py
@@ -33,4 +33,5 @@ def servidor():
       respuesta = (
         "HTTP/1.1 403 Forbidden\r\n"
         "Content-Type: text/html\r\n\r\n"
+        "<html><body><h1>403 Acceso Denegado</h1></body></html>"
 
